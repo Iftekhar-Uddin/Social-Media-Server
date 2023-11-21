8f98141d0484bf3b3b3f227a7fbd8d9a0267d3e7
Iftekhar Uddin <iftekharuddin720@gmail.com>
2023-11-21 10:26:13 +0600

    hello

---
+++ middleware/routes/auth.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+const router = express.Router();
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {
+      //generate new password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  
+      //create new user
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+  
+      //save user and respond
+      const user = await newUser.save();
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error)
+    }
+});
+
+
+// LOGIN
+router.post('/login', async (req, res)=>{
+  try {
+    const  user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(404).json("user not found");
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).json("wrong password");
+    const token = jwt.sign({username: user.username, id: user._id}, 'mind', {expiresIn: "1h"});
+    res.status(200).json({result: user, token});
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+export default router;
---
+++ middleware/routes/conversations.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import Conversation from '../models/Conversation.js'
+const router = express.Router();
+
+// new conversations
+router.post('/', async (req, res) => {
+    const newConversation = new Conversation({
+        members: [req.body.senderId, req.body.receiverId],
+    });
+    try {
+        const savedConversation = await newConversation.save();
+        res.status(200).json(savedConversation)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+
+// get conversation of a user
+router.get('/:userId', async (req, res) => {
+    try {
+        const conversations = await Conversation.find({
+            members: { $in: [req.params.userId]}
+        });
+        res.status(200).json(conversations)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+
+// get conversation includes two userId
+router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
+    try {
+        const conversation = await Conversation.findOne({
+            members: {$all: [req.params.firstUserId, req.params.secondUserId]}
+        });
+        res.status(200).json(conversation)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+export default router;
